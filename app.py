a9e29dff3c661637d5c6cd2411624c4313cdf185
exceldra5 <exceldra5@gmail.com>
2024-05-20 00:20:17 +0900

    upgrade design

--- app.py
+++ app.py
@@ -40,17 +40,68 @@ db = load_data()
 
 st.title("Ask to Sooyong")
 
-user_input = st.text_input("신수용에 대해 궁금한 점을 물어보세요")
+if "chat_messages" not in st.session_state:
+    st.session_state.chat_messages = [
+        {
+            "role": "bot",
+            "content": "안녕하세요 저는 신수용 입니다. 제게 궁금한 점들을 물어보세요",
+        }
+    ]
 
-# Button
+chat_messages = st.session_state.chat_messages
+
+
+def display_chat_messages(chat_messages):
+    chat_html = """
+    <div class="chat-container">
+    """
+    for msg in chat_messages:
+        if msg["role"] == "user":
+            chat_html += f"<div class='user-message'>{msg['content']}</div>"
+        elif msg["role"] == "bot":
+            chat_html += f"<div class='bot-message'>{msg['content']}</div>"
+    chat_html += "</div>"
+    return chat_html
+
+
+with open("styles.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+chat_container = st.empty()
+
+chat_container.markdown(display_chat_messages(chat_messages), unsafe_allow_html=True)
+
+user_input = st.text_input("")
+
 if st.button("전송"):
     if user_input:
-        # Retrieval
-        llm = ChatOpenAI(model_name="gpt-4o", temperature=0.5)
-        qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
-        result = qa_chain({"query": user_input})
+        chat_messages.append({"role": "user", "content": user_input})
+        st.session_state.chat_messages = chat_messages
 
-        # Output
-        st.write(result["result"])
+        chat_container.markdown(
+            display_chat_messages(chat_messages), unsafe_allow_html=True
+        )
+
+        # Retrieve response from the model
+        with st.spinner("답변을 생성중입니다..."):
+            llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+            qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+            result = qa_chain(
+                {
+                    "query": "Imagine you are Sooyong Shin. The user will send you a message. Please respond as Sooyong Shin would, in a formal tone and conversational manner. If the user asks about your studies or achievements, provide detailed, personal insights. Make sure the length does not exceed 200 characters unless essential information needs to be included. Do not include a formal opening or closing. User:"
+                    + user_input
+                }
+            )
+
+        answer = result["result"]
+
+        chat_messages.append({"role": "bot", "content": answer})
+        st.session_state.chat_messages = chat_messages
+
+        chat_container.markdown(
+            display_chat_messages(chat_messages), unsafe_allow_html=True
+        )
+
+        st.session_state.user_input = ""
     else:
         st.write("질문을 입력해주세요")
